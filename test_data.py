fd1bdd990e7bccb16cd7c78c18e3f93e99a7205a
Gustavo Trigos <gustavoa.trigos@Guss-MacBook-Pro.local>
2020-03-17 14:35:06 -0700

    added more stats to test_data.py

--- test_data.py
+++ test_data.py
@@ -2,7 +2,6 @@ from fetch_data import df_confirmed, df_death, df_recovered
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 def data_type(data):
 	# get metadata (columns that describe the timeseries data)
 	metadata = data.iloc[:,:4]
@@ -15,13 +14,21 @@ def data_type(data):
 # Use case for Confirmed
 metadata, timeseries = data_type(df_confirmed)
 
-country = "Mexico"
+country = "US"
 
 # data of a particular country
-country_data = timeseries.loc[country]
+country_data = timeseries.loc[country].sum(axis=0)
 country_data.index = pd.to_datetime(country_data.index)
 
-plt.title("Confirmed cases in {country} as of {today}".format(country=country,today=country_data.index[-1].date()))
+today = country_data.index[-1].date()
+
+# percentage change daily
+print(country_data.pct_change(1))
+
+# up to date data
+print("data as of {}: ".format(today),country_data.iloc[-1])
+
+plt.title("Confirmed cases in {country} as of {today}".format(country=country,today=today))
 plt.xticks(rotation=45)
 plt.ylabel("Confirmed Cases")
 plt.xlabel("Date")
